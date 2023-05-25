7b8091460d5b5aca705b4258c2b27f861de0e5c1
Nandhu Jayan <nandhu.22pmc139@mariancollege.org>
2023-05-25 20:08:54 +0530

    initial commit

---
+++ blog/utils.py
@@ -0,0 +1,13 @@
+def is_ajax(request):
+    """
+    To fix request.is_ajax() error which is deprecated in django > v3.1
+
+    Args:
+        request (request)
+
+    Returns:
+        _type_: boolean
+    """
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+# like button is working becuase of this
